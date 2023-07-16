dbffc3402b270b836da3f2fa131ce4e5cb5bae53
avi1998s <avi6190i@gmail.com>
2023-07-16 17:43:39 +0300

    .

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './style.module.css'
+const Login = () => {
+    return (
+        <div className='center'>
+            <h2>login</h2>
+        </div>
+    );
+}
+
+export default Login;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
 
 export default function MainRouter() {
+   const [user, setUser] = useState(false);
+   useEffect(()=>{
+
+   },[])
+   const getUser = ()=>{
+      setUser(localStorage.getItem("user")?localStorage.getItem("user"):false)
+   }
    return (
       <Routes>
-         <Route/>
+         <Route
+          path="/"
+          element={
+          <MainLayout
+            active={false}
+            contentChilden={<div>main active</div>}
+            navChilden={<div>main active</div>}
+          />
+         }
+          />
+
       </Routes>
    )
 }
