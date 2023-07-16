656f43bd693b95d2ef52194c425502ac464462e2
sagi buta <sagibuta@gmail.com>
2023-07-16 15:44:51 +0300

    creat first context witout data

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
+import { createContext } from "react"
+
+const DataContext = createContext({})
+
 
 export default function MainContext({ children }) {
+
+   const [fakeData, setFakeData] = useState()
+   const [action, setAction] = useState()
+   const [settingData, setSetingData] = useState()
+
+
    return (
-      <>{children}</>
+      <DataContext.Provider value={{ fakeData, setFakeData, action, setAction, settingData, setSetingData }} >
+         <>{children}</>
+      </DataContext.Provider>
    )
 }
