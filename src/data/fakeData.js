ad985205719ae1fc36ec148532015d382ef9b9be
sagi buta <sagibuta@gmail.com>
2023-07-16 19:23:57 +0300

    edit data

--- src/data/fakeData.js
+++ src/data/fakeData.js
@@ -2,18 +2,80 @@
 export const actions = [
    {
       _id: "null",
-      orderSource: null,
-      fundingSource: null,
-      location: null,
-      locationType: null,
-      days: [1, 3, 5],
-      startTime: null,
-      endTime: null,
-      startDate: null,
-      endDate: null,
-      status: null,
-      lecturer: null,
+      orderSource: "עריית בני ברק",
+      fundingSource: "משרד הכלכלה",
+      location: "בני ברק",
+      locationType: 1,
+      days: [1, 5],
+      startTime: "16:30",
+      endTime: "21:30",
+      startDate: new Date("01/01/2023"),
+      endDate: new Date("01/08/2023"),
+      status: 1,
+      lecturer: "yosef",
       actionType: 'workshop',
+      name: 'full stack',
+      files: [
+         {
+            fileName: "syllabus-fs",
+            fileType: 'txt',
+            size: "2433",
+            createdDate: new Date("12/04/2022")
+         },
+         {
+            fileName: "images01",
+            fileType: 'jpg',
+            size: "202131",
+            createdDate: new Date("11/06/2023")
+         }
+      ],
+      tasks: [
+         {
+            deadline: new Date("22/07/2023"),
+            details: "task 1",
+            isDone: false,
+         },
+         {
+            deadline: new Date("23/07/2023"),
+            details: "task 2",
+            isDone: true,
+         },
+         {
+            deadline: new Date("27/07/2023"),
+            details: "task 3",
+            isDone: false,
+         },
+      ],
+      users: ["123", "456", "789"],
+      schedules: [
+         {
+            date: "16/07/2023",
+            lecturer: "Aviad",
+            comments: "to do users name",
+            status: "active"
+         },
+         {
+            date: "20/08/2023",
+            lecturer: "Aviad",
+            comments: "to do users  last name",
+            status: "active"
+         }
+      ]
+   },
+   {
+      _id: "null",
+      orderSource: "מועצה איזורית מטה בינימין",
+      fundingSource: `מגע"ר`,
+      location: "טלמון",
+      locationType: 2,
+      days: [1, 4],
+      startTime: new Date("09:00"),
+      endTime: new Date("16:00"),
+      startDate: new Date("10/11/2023"),
+      endDate: new Date("10/11/2024"),
+      status: 2,
+      lecturer: "אביעד",
+      actionType: 'course',
       name: 'full stack',
       files: [
          {
@@ -31,38 +93,44 @@ export const actions = [
       ],
       tasks: [
          {
-            dealine: new Date("22/07/2023"),
+            deadline: new Date("22/07/2023"),
             details: "task 1",
             isDone: false,
          },
          {
-            dealine: new Date("23/07/2023"),
+            deadline: new Date("23/07/2023"),
             details: "task 2",
             isDone: true,
          },
          {
-            dealine: new Date("27/07/2023"),
+            deadline: new Date("27/07/2023"),
             details: "task 3",
             isDone: false,
          },
       ],
-      users: ["123", "456"],
+      users: ["123", "456", "789"],
       schedules: [
          {
-            date: "18/08/2023",
+            date: "09/07/2023",
             lecturer: "Aviad",
             comments: "to do users name",
             status: "active"
          }
+         , {
+            date: "12/07/2023",
+            lecturer: "Aviad",
+            comments: "to do users  last name",
+            status: "active"
+         }
       ]
-   },
+   }
 
 ]
 
 // ** משתמשים / סטודנטים **
 export const users = [
    {
-      _id: "0",
+      _id: "1",
       userId: "123",
       fName: 'Avi',
       lName: "Sason",
@@ -73,7 +141,7 @@ export const users = [
       permission: "student"
    },
    {
-      _id: "1",
+      _id: "2",
       userId: "456",
       fName: 'Ben',
       lName: "Cohen",
@@ -84,7 +152,7 @@ export const users = [
       permission: "student",
    }
    , {
-      _id: "2",
+      _id: "3",
       userId: "789",
       fName: 'sagi',
       lName: "buta",
@@ -96,12 +164,45 @@ export const users = [
    }
 ]
 
-// export const settings = { actionType: { workshop: { colors: "" }, course: "" } }
+export const actionTypes = [{
+   _id: "111qqq",
+   actionType: "workshop",
+   nameHebrew: "סדנה",
+   color: ""
+},
+{
+   _id: "222qqq",
+   actionType: "course",
+   nameHebrew: "קורס",
+   color: "",
+}
+]
+
+export const days =
+{
+   day1: "'א",
+   day2: "'ב",
+   day3: "'ג",
+   day4: "'ד",
+   day5: "'ה",
+   day6: "'ו",
+   day7: "'ז",
+}
+
+
+export const orderSource = ["עריית בני ברק", "מועצה אזורית מטה בנימין"]
+
+export const fundingSource = ["משרד הכלכלה", `מגע"ר`]
+
 
 
 // TODO :
-// - settings
-// ---- object of  actionType  { color, nameHebrew , _id}
+
 // ---- days - number to day (hebrew) {1 : "'א"}
+
+
+
+// ---- object of  actionType  { color, nameHebrew , _id}
+// - settings
 // ---- orderSource = ["",""]
 // ---- fundingSource = ["",""]
