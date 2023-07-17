e4949c551e04f07d9736b935f8c04cfdd3ad3396
johny11111 <125285264+johny11111@users.noreply.github.com>
2023-07-17 14:21:09 +0300

    globalcss

---
+++ src/test/Css.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Css() {
+   return (
+      // <div>Yonatan</div>
+      <div className='scroller'>Yonatan
+         <h1 >yonatan</h1>
+         <h2 >yonatan</h2>
+         <h3 >yonatan</h3>
+         <h4 >yonatan</h4>
+         <p>w40</p>
+         <button className='w40'>...</button><br />   
+         <p>w55</p>
+         <button className='w55'>א</button>
+         <p>w94</p>
+         <button className='w94'>קורס</button>
+         <p>w140</p>
+         <button className='w140'>שמור</button>
+         <p>w154</p>
+         <button className='w154'>פעילויות נוספות</button>
+         <p>w166</p>
+         <button className='w166'>נחתמה</button><br /><br />
+         <p>w210</p>
+         <button className='w210'>תאריך פתיחה 10/08/2023</button><br />
+         <p>w300</p>
+         <button className='w300'>click</button>
+
+      </div>
+   )
+}
--- src/test/Yonatan.jsx
+++ src/test/Yonatan.jsx
@@ -2,23 +2,7 @@ import React from 'react'
 
 export default function Yonatan() {
    return (
-      // <div>Yonatan</div>
-      <div>Yonatan
-         <h1 >yonatan</h1>
-         <h2 >yonatan</h2>
-         <h3 >yonatan</h3>
-         <h4 >yonatan</h4>
-         <button className='w40'>...</button>
-         <button className='w94'>קורס</button>
-         <button className='w110'>מחשבים</button>
-         <button className='w140'>שמור</button>
-         <button className='w154'>פעילויות נוספות</button>
-         <button className='w166'>נחתמה</button>
-         <button className='w210'>תאריך פתיחה 10/08/2023</button>
-         <button className='w244'></button>
-         <button className='w300'>click</button>
-      
-
-      </div>
+     
+      <div>yonatan</div>
    )
 }
