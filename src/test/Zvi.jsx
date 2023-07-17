bf92b67dec8540264e4bf7cc719048d398142bdc
ZviMiler <zvimiler@outlook.co.il>
2023-07-17 13:38:23 +0300

    componnt common

--- src/test/Zvi.jsx
+++ src/test/Zvi.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Common from '../components/Common';
 
 export default function Zvi() {
+   const[file,setFile]=useState([]);
+   // function hndleFrom(e){
+   //    e.preventDefault();
+   //    const file=e.target.myfile.value;
+   //    console.log(file);
+   // }
    return (
-      <div>Tzvi</div>
+      <div>
+      <form>
+
+         <Common stet={file} setStet={setFile}/>
+         </form>
+         {console.log(file)}
+      </div>
    )
 }
