d87ebd3a96d461936ab5268099a649a6f8323656
ItamarFadel <itamarfadel1991@gmail.com>
2023-07-17 14:22:20 +0300

    hours components

--- src/test/Itamar.jsx
+++ src/test/Itamar.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Itamar() {
-   return (
-      <div>Itamar</div>
-   )
+const [start , setstart]= useState ([])
+const [finish , setfinish]= useState ([])
+
+const hours = [2,4,6,8]
+
+function CangeNumber(e){
+
+console.log  (e.targt.value)
+
 }
+
+
+
+
+
+
+
+
+
+return(
+
+
+   <div>
+      <div>
+        <label>שעות לימוד</label>
+  <select onChange={(e)=>CangeNumber(e)} name="hours1" id="abc">
+
+   {hours.map((item,i)=> {
+   return <option >{item}</option>
+   }
+)}
+  </select>
+  </div>
+  <br />
+<div>
+<label>שעות סיום</label>
+  <select name="hours1" id="abc">
+
+   {hours.map((item,i)=> {
+   return <option className={item}>{item}</option>
+   }
+)}
+  </select>
+
+   
+
+
+
+</div>
+
+
+
+  </div>
+
+
+      )      
+   
+
+
+
+
+
+
+
+
+
+}
+
+    
