5b896c64eec1fe0992b73348eddf66de7898f437
eladm <eladmiz2046@gmail.com>
2023-07-17 09:39:52 +0300

    input text is red prepard

--- src/test/Elad.jsx
+++ src/test/Elad.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InputText from '../components/InputText'
 
 export default function Elad() {
+   const [inputValue, setInputValue] = useState([{}]);
+
+   const handleInputChange = (event) => {
+      setInputValue(event.target.value)
+   };
+
+   const handleSubmit = (event) => {
+      event.preventDefault();
+      let fname = event.target.fname.value
+      let lname = event.target.lname.value
+      let age = event.target.age.value
+
+      console.log(fname,lname,age);
+   };
    return (
-      <div>Elad</div>
+      <form onSubmit={handleSubmit}>
+         <InputText onChange={handleInputChange} name ="fname" placeholder="fname"/>
+         <InputText onChange={handleInputChange} name="lname" placeholder="lname"/>
+         <InputText onChange={handleInputChange} name="age" placeholder="age"/>
+
+         <button type="submit">Submit</button>
+      </form>
    )
 }
