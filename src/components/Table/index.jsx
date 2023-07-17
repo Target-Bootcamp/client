cee55ea57bee03a85ddba3db9e18ea5676e7f5e6
asher shnur <rachel0548540990@gmail.com>
2023-07-17 20:59:32 +0300

    cheked table

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,24 +1,27 @@
-// import styles from './style.module.css'
+import styles from './style.module.css'
 import { BsTrash, BsFillPencilFill } from "react-icons/Bs";
 
 export default function Table(props) {
    let obj = props.arr ?? Object.keys(props.data[0])
- 
+
    function tempFunck(objeckt) {
       return <tr> {obj.map(key => {
-         return <td>{objeckt[key]}</td>
-      })}  {props.deletion ? <td><button onClick={() => props.deletion(objeckt)}><BsTrash /></button></td> : null}
-         {props.editing ? <td><button onClick={() => props.editing(objeckt)}><BsFillPencilFill /></button></td> : null}
+         return <>
+            {key === "isDone" ? <td>{<input type="checkbox" className='check' checked={objeckt[key]} />}</td> : <td>{objeckt[key]}</td>}</>
+
+      })}  {props.deletion ? <td><button onClick={() => props.deletion(objeckt)}><BsTrash className="icons" /></button></td> : null}
+         {props.editing ? <td><button onClick={() => props.editing(objeckt)}><BsFillPencilFill className="icons" /></button></td> : null}
 
       </tr>
    }
-   
-   return (<table>{obj.map(val => <th>{val}</th>)}
+
+   return (
+      <table className='tw'>{obj.map(val => <th>{val}</th>)}
 
 
-      {props.data.map((value) => tempFunck(value)
-      )}
+         {props.data.map((value) => tempFunck(value)
+         )}
 
 
-   </table>)
+      </table>)
 }
