7eb08db0f59494c5708a3a5402466c38a4aa28d3
Efraim <Nr224808@gmail.com>
2023-07-17 19:08:56 +0300

    search folder function search

--- src/test/Efraim.jsx
+++ src/test/Efraim.jsx
@@ -1,7 +1,58 @@
 import React from 'react'
+import Search from './Search'
 
-export default function Efraim() {
+
+
+function Efraim() {
    return (
-      <div>Efraim</div>
+      <div>
+         <Search placeholder={"גוף מזמין..."} data={"fundingSource"} />
+         <Search placeholder={"גוף מממין..."} data={"orderSource"} />
+
+      </div>
+
    )
 }
+
+export default Efraim
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react'
+// // import Search from './Search'
+// import products from './products.json'
+
+// export default function Efraim() {
+//    const [value, setValue] = useState('')
+//    const [json] = [products]
+
+//    const onChange = (event => {
+//       setValue(event.target.value)
+//    })
+//    return (
+//       <div>
+//          <input type="text" value={value} onChange={onChange}
+//          // onInput={(event) => event.target.value.length > 2 ? <Search val={event.target.value} /> : null}
+//          />
+//       </div>
+//    )
+// }
+
