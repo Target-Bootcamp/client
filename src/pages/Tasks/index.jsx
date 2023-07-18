a760afe11db3074cf29f5ef8be07cbf9e88e79f7
Efraim <Nr224808@gmail.com>
2023-07-18 11:56:56 +0300

    start to do the webPage

--- src/pages/Tasks/index.jsx
+++ src/pages/Tasks/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import styles from './style.module.css'
-import { useParams } from 'react-router';
+import { useParams } from 'react-router'
+import { useState } from "react"
+import { useContext } from 'react'
+import { DataContext } from '../../context/index'
+// import Table from '../../components/Table';
+import PageHeader from '../../components/pageHeader';
 
 const Tasks = () => {
     const params = useParams()
+    const allData = useContext(DataContext).actions.find(i => i.lecturer == params.id)
+
+    console.log(allData);
     return (
         <div className={`center ${styles.tasks}`}>
+            {/* <Table props={allData.files} /> */}
+            <PageHeader pageName={params.id} actionType={allData.actionType} />
             tasks {params.id} page
         </div>
     );
