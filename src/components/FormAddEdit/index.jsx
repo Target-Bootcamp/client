ba1ae906e38a58e9ae15c4e4baea85e893b7e43c
johny11111 <125285264+johny11111@users.noreply.github.com>
2023-07-18 17:51:01 +0300

    formAddEdit

--- src/components/FormAddEdit/index.jsx
+++ src/components/FormAddEdit/index.jsx
@@ -1,39 +1,54 @@
-import React from 'react'
+import React from 'react';
 import styles from "./style.module.css";
-import InputText from "../InputText"
-import Button from "../Button"
-import { useParams } from 'react-router-dom';
+import InputText from "../InputText";
+import Button from "../Button";
 
+// creator : yonatan ben david
 
 export default function FormAddEdit(props) {
-
   function newStudent(student) {
-    let name = student.target.name.value
-    let lastName = student.target.lastName.value
-    let phoneNumber = student.target.phoneNumber.value
-    let email = student.target.email.value
+    const newStudent = {
+      name: student.target.name.value,
+      lastName: student.target.lastName.value,
+      phoneNumber: student.target.phoneNumber.value,
+      email: student.target.email.value
+    };
+    console.log(newStudent);
   }
 
   function UpdateStudent(studentToUpdate) {
     console.log("student to update: ", studentToUpdate);
   }
+
   return (
     <div className={styles.containerFrom}>
-      <form onSubmit={e => {
+      <form className={styles.form} onSubmit={e => {
         e.preventDefault();
-        if (!props) {
-          return UpdateStudent(props)
-        }
-        else {
-          newStudent(e)
-        }
+        props.userToUpdate ?  UpdateStudent(e) : newStudent(e);
       }}>
-        <InputText name={"name"} placeholder={"first name"} defaultValue={props.fName} />
-        <InputText name={"lastName"} placeholder={"last name,"} />
-        <InputText name={"phoneNumber"} type={"number"} placeholder={"phone number"} />
-        <InputText name={"email"} placeholder={"email"} />
+        <InputText
+          name={"name"}
+          placeholder={!props.userToUpdate ? "Enter first name" : ""}
+          defaultValue={props.userToUpdate ? props.userToUpdate.name : ""}
+        />
+        <InputText
+          name={"lastName"}
+          placeholder={!props.userToUpdate ? "Enter last name" : ""}
+          defaultValue={props.userToUpdate ? props.userToUpdate.lastName : ""}
+        />
+        <InputText
+          name={"phoneNumber"}
+          type={"number"}
+          placeholder={!props.userToUpdate ? "Enter phone number" : ""}
+          defaultValue={props.userToUpdate ? props.userToUpdate.phone : ""}
+        />
+        <InputText
+          name={"email"}
+          placeholder={!props.userToUpdate ? "Enter email" : ""}
+          defaultValue={props.userToUpdate ? props.userToUpdate.email : ""}
+        />
         <Button butoonName={"שלח"} type={"submit"} className="w110" />
       </form>
     </div>
-  )
+  );
 }
