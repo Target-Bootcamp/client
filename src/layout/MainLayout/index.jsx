6b0273e8edc04901314e597dcfd87d1e4cf0ba23
sagi buta <sagibuta@gmail.com>
2023-07-18 19:09:43 +0300

    popup

--- src/layout/MainLayout/index.jsx
+++ src/layout/MainLayout/index.jsx
@@ -1,20 +1,22 @@
 import React, { useContext, useEffect } from 'react';
-import styles from './style.module.css' 
+import styles from './style.module.css'
 import SideNav from '../SideNav';
 import Content from '../Content';
-import {DataContext} from '../../context/index'
+import { DataContext } from '../../context/index'
+import Popup from '../Popup';
 
-const MainLayout = () => {   
+const MainLayout = () => {
     const context = useContext(DataContext);
     const setActivety = context.setIsActive;
-    console.log(context);
-    useEffect(()=>{
-        window.location.pathname==="/"?setActivety(false):"";
-    },[]) 
+
+    useEffect(() => {
+        window.location.pathname === "/" ? setActivety(false) : "";
+    }, [])
     return (
         <div className={`center ${styles.hiro}`}>
-            <SideNav/>
-            <Content/> 
+            <SideNav />
+            <Content />
+            {/* {context.popUp && <Popup />} */}
         </div>
     );
 }
