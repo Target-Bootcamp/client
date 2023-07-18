74db2c9a46ff1f6bf41d75869c237a3d7cdafd54
avi1998s <avi6190i@gmail.com>
2023-07-18 12:18:56 +0300

    test fixd

--- src/layout/Content/index.jsx
+++ src/layout/Content/index.jsx
@@ -1,20 +1,24 @@
 import React, { useContext, useEffect, useState } from 'react';
 import styles from './style.module.css' 
 import { Route, Routes } from 'react-router-dom';
+import {DataContext} from '../../context/index'
+import MainTest from '../../test';
+// pages
 import Action from '../../pages/Action';
 import NewAction from '../../pages/NewAction';
 import Files from '../../pages/Files';
 import Schedule from '../../pages/Schedule';
 import Tasks from '../../pages/Tasks';
 import Students from '../../pages/Students';
-import {DataContext} from '../../context/index'
 const  Content = ({style={}}) => {
   const context = useContext(DataContext);
   const activety = context.isActive
   const setActivety = context.setIsActive
   
+  
   return (
     <div className={`center ${styles.content} ${activety?styles.contentActive:styles.contentNoActive}`} style={style}>
+      <MainTest/>
       <Routes>
         <Route path='/' element={<div>main page</div>} />
         <Route path='/action' element={<NewAction />} />
