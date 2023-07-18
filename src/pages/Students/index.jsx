672a28a3cf0e1e5a6f49765e07e9fa6feb05d22f
johny11111 <125285264+johny11111@users.noreply.github.com>
2023-07-18 12:05:18 +0300

    student

--- src/pages/Students/index.jsx
+++ src/pages/Students/index.jsx
@@ -1,21 +1,36 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import styles from './style.module.css'
-import { useParams } from 'react-router-dom';
-import {DataContext} from '../../context/index'
-const Students = () => {
-    const params = useParams()
-    const context = useContext(DataContext);
-    const activety = context.isActive;
-    const setActivety = context.setIsActive;
-    useEffect(()=>{
-        setActivety(true)
-    },[])
+import { useParams } from 'react-router';
+import { DataContext } from '../../context/index';
+import Table from '../../components/Table';
+import PageHeader from '../../components/PageHeader';
 
-    return (
-        <div className={`center ${styles.students}`}>
-            students {params.id} page
-        </div>
-    );
+const Students = () => {
+    const valueContext = useContext(DataContext);
+    const params = useParams();
+    const param = parseInt(params.id);
+
+    if (param >= 0 && param < valueContext.actions.length) {
+        const activity = valueContext.actions[param];
+        const activityType = activity.actionType;
+        const students = valueContext.users.filter(user => activity.users.includes(user.userId));
+
+        return (
+            <div>
+                <div className={`${styles.students}`}>
+                    <PageHeader actionType={activityType} pageName={"תלמידים"} />
+                    <Table data={students} />
+                </div>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div className={styles.students}>
+                <h1>פעילות לא נמצאה</h1>
+            </div>
+        )
+    }
 }
 
 export default Students;
