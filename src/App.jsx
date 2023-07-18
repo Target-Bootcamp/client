ee357950e6ce822db440d3a73969a605a6ed7c30
avi1998s <avi6190i@gmail.com>
2023-07-18 11:50:05 +0300

    test fixd

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <>
       <MainContext>
         <MainRouter />
+        {/* <MainTest/> */}
       </MainContext>
     </>
   )
--- src/layout/Content/index.jsx
+++ src/layout/Content/index.jsx
@@ -8,6 +8,22 @@ import Schedule from '../../pages/Schedule';
 import Tasks from '../../pages/Tasks';
 import Students from '../../pages/Students';
 import {DataContext} from '../../context/index'
+// testing----------------------------------------------------------------
+import Aviad from '../../test/Aviad'
+import Shay from '../../test/Shay'
+import Tzvi from '../../test/Zvi'
+import Moyshi from '../../test/Moyshi'
+import Eli from '../../test/Eli'
+import Yonatan from '../../test/Yonatan'
+import Avraham from '../../test/Avraham'
+import Sagi from '../../test/Sagi'
+import Efraim from '../../test/Efraim'
+import Asher from '../../test/Asher'
+import Elad from '../../test/Elad'
+import Amram from '../../test/Amram'
+import Itamar from '../../test/Itamar'
+import Inon from '../../test/Inon'
+
 const  Content = ({style={}}) => {
   const context = useContext(DataContext);
   const activety = context.isActive
@@ -23,6 +39,21 @@ const  Content = ({style={}}) => {
         <Route path='/action/:id/students' element={<Students />} />
         <Route path='/action/:id/schedule' element={<Schedule />} />
         <Route path='/action/:id/tasks' element={<Tasks />} />
+        {/* testing */}
+        <Route path='/aviad' element={<Aviad />} />
+         <Route path='/shay' element={<Shay />} />
+         <Route path='/tzvi' element={<Tzvi />} />
+         <Route path='/moyshi' element={<Moyshi />} />
+         <Route path='/eli' element={<Eli />} />
+         <Route path='/yonatan' element={<Yonatan />} />
+         <Route path='/avraham' element={<Avraham />} />
+         <Route path='/sagi' element={<Sagi />} />
+         <Route path='/efraim' element={<Efraim />} />
+         <Route path='/asher' element={<Asher />} />
+         <Route path='/elad' element={<Elad />} />
+         <Route path='/amram' element={<Amram />} />
+         <Route path='/itamar' element={<Itamar />} />
+         <Route path='/inon' element={<Inon />} />
       </Routes>
     </div>
   );
