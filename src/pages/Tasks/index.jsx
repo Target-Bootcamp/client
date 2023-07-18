471b3ac3c1b5b5e1bf24c9664f1010825bec61b5
Efraim <Nr224808@gmail.com>
2023-07-18 16:25:35 +0300

    stop for to sec

--- src/pages/Tasks/index.jsx
+++ src/pages/Tasks/index.jsx
@@ -2,6 +2,10 @@ import React, { useContext, useEffect } from 'react';
 import styles from './style.module.css'
 import { useParams } from 'react-router-dom';
 import { DataContext } from '../../context/index'
+import PageHeader from '../../components/PageHeader'
+import Table from '../../components/Table'
+import Button from '../../components/Button'
+
 
 const Tasks = () => {
     const context = useContext(DataContext);
@@ -11,16 +15,20 @@ const Tasks = () => {
         setActivety(true)
     }, [])
     const params = useParams()
-    const allData = useContext(DataContext).actions.find(i => i.lecturer == params.id)
+    const allData = useContext(DataContext).actions.find(i => i.lecturer === params.id)
 
-    console.log(allData);
+    console.log(allData.files);
     return (
         <div className={`center ${styles.tasks}`}>
             {/* <Table props={allData.files} /> */}
-            <PageHeader pageName={params.id} actionType={allData.actionType} />
-            tasks {params.id} page
+            <div><PageHeader pageName={"משימות לביצוע"} actionType={allData.actionType} /> </div>
+            <div>{/* tasks {params.id} page */}</div>
+            <div><Button className="w140" /* onClick={(onClick)}     type={type}*/ butoonName="שמור" /></div>
+
         </div>
     );
 }
 
 export default Tasks;
+
+
