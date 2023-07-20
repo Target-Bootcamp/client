52a28287da6a5037203e0b610ec6f58096684d05
ItamarFadel <itamarfadel1991@gmail.com>
2023-07-20 09:55:51 +0300

    finish to create hovers

--- src/test/Itamar.jsx
+++ src/test/Itamar.jsx
@@ -1,72 +1,12 @@
+
 import React, { useState } from 'react'
+import generateHours from '../functions/generateHours'
+import InputHours from '../components/InputHours'
+
 
 export default function Itamar() {
-const [start , setstart]= useState ([])
-const [finish , setfinish]= useState ([])
-
-const hours = [2,4,6,8]
-
-function CangeNumber(e){
-
-console.log  (e.targt.value)
-
-}
+   return(
+      <InputHours/>
+   )}
 
 
-
-
-
-
-
-
-
-return(
-
-
-   <div>
-      <div>
-        <label>שעות לימוד</label>
-  <select onChange={(e)=>CangeNumber(e)} name="hours1" id="abc">
-
-   {hours.map((item,i)=> {
-   return <option >{item}</option>
-   }
-)}
-  </select>
-  </div>
-  <br />
-<div>
-<label>שעות סיום</label>
-  <select name="hours1" id="abc">
-
-   {hours.map((item,i)=> {
-   return <option className={item}>{item}</option>
-   }
-)}
-  </select>
-
-   
-
-
-
-</div>
-
-
-
-  </div>
-
-
-      )      
-   
-
-
-
-
-
-
-
-
-
-}
-
-    
