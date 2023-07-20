caf7cdb61fc709ddea42c16320943a3149ea2863
oshri <omeroshri101@gmail.com>
2023-07-20 12:26:47 +0300

    my compenent is working successfully

--- src/components/selectButtons/index.jsx
+++ src/components/selectButtons/index.jsx
@@ -1,21 +1,37 @@
 import { useEffect, useState } from "react";
 import styles from './style.module.css'
 
+// this compenent has a functuality to choose the place of the studies (external or clients) only one of them,
+//the sort of the studies (course, whorshop or prep) only one of them,
+// the days of studies which this choise is multiple
+// second click of each button will remove the choise
+
 export default function SelectButtons({ options, isSingleChoice, className }) {
-    const [choice, setChoice] = useState();
+    const [selected, setSelected] = useState([]);
+
+    const handleClick = (v) => {
+
+        setSelected(
+            selected.includes(v) ?
+                selected.filter(s => s != v)
+                : (isSingleChoice ? [v] : [...selected, v]))
+    }
+
     useEffect(() => {
-        console.log(choice);
-    }, [choice])
-    
+        console.log(selected);
+    }, [selected])
 
     return (
         <div>
-            {options.map(val => val.map(v => {
-                return <button className={`${choice === v.value ? styles.activee : ""}
-                       ${styles.select} ${className}`} key={v.value} onClick={() =>
-                        setChoice(v.value)}> {v.text} </button>
-            }))
+            {options.map(v => {
+                return <button className={`${selected.includes(v.value) ? styles.activee : ""}
+                 ${styles.select} ${className}`} key={v.value}
+                    onClick={() => handleClick(v.value)} > {v.text} </button>
+            })
             }
         </div>
     )
+
+
+   
 }
