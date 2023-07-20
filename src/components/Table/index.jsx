2ba1a8049cb16a5584011c54dbc2171cc929603b
johny11111 <125285264+johny11111@users.noreply.github.com>
2023-07-20 22:49:09 +0300

    student table css

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,5 +1,6 @@
 import styles from './style.module.css'
-import { BsTrash, BsFillPencilFill } from "react-icons/bs";
+import {MdOutlineDelete  } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
 import { useContext } from 'react';
 import { DataContext } from '../../context';
 import FormAddEdit from '../FormAddEditStudent';
@@ -27,9 +28,9 @@ export default function Table({ data, arr, deletion, editing, TaskMarking, add }
                : <td>{objeckt[key]}</td>}</>
 
       })}  {deletion ? <td><button onClick={() => deletion(objeckt)}>
-         <BsTrash className={styles.icons} /></button></td> : null}
+         <MdOutlineDelete className={styles.icons} /></button></td> : null}
          {editing ? <td><button onClick={() => editing(objeckt)}>
-            <BsFillPencilFill className={styles.icons} /></button></td> : null}
+            <CiEdit className={styles.icons} /></button></td> : null}
       </tr>
    }
 
--- src/pages/Students/index.jsx
+++ src/pages/Students/index.jsx
@@ -7,17 +7,18 @@ import PageHeader from '../../components/PageHeader';
 import FormAddEditStudent from '../../components/FormAddEditStudent';
 import CurrentAction from '../../components/CurrentAction';
 import apiCalls from '../../functions/apiCalls';
+import InputText from '../../components/InputText';
 
 // creator : yonatan ben david
 const Students = () => {
-    const action = CurrentAction;
+
     const [arryStundent, setArryStundent] = useState([])
     const [data, setData] = useState()
 
 
     const params = useParams();
     const param = params.actionId;
-    
+
     useEffect(() => {
         apiCalls.get(`/actions/${param}`)
             .then(res => {
@@ -41,8 +42,8 @@ const Students = () => {
             comments: null,
             permission: null,
         };
-        apiCalls.post("/", { newStudent })
-            .then(res => console.log(res.data))
+        apiCalls.post("/", newStudent)
+            .then(req => console.log(req))
             .catch(err => console.log(err.message));
 
         console.log(newStudent);
@@ -62,29 +63,33 @@ const Students = () => {
     }
 
     const valueContext = useContext(DataContext);
-    // data && console.log(data[0]._id);
 
-    // const activityId = data.users.map(activity => activity);
-    // if (activityId) {
-    //     activityId.map(student => {
-    //         apiCalls.get(`/users/${student}`)
-    //             .then(response => console.log(response))
-    //             .catch(error => console.log(error))
-    //     })
-    // // }
-    // arryStundent && console.log("arry", arryStundent);
     return (
         <div>
-            {!data ? <div className={`${styles.students}`}>
-                <div>work with data</div>
-                {/* <PageHeader actionType={data.activityType} pageName={"תלמידים"} /> */}
-                {/* <Table deletion={"del"} data={arryStundent} add={obj => valueContext.setPopUp(<FormAddEditStudent newStudent={newStudent} UpdateStudent={UpdateStudent} />)} editing={obj => valueContext.setPopUp(<FormAddEditStudent userToUpdate={obj} UpdateStudent={UpdateStudent} newStudent={newStudent} />)} /> */}
-            </div>
-                :
-                <div className={styles.students}>
-                    <h1>פעילות לא נמצאה</h1>
-                </div>}
+            <CurrentAction />
+            <div>
 
+
+                {data ? <div className={`${styles.students}`}>
+                    <div className={`${styles.containerHeader}`}>
+                        <PageHeader actionType={data.actionType} pageName={"תלמידים"} />
+                    </div>
+                    <div className='start'>
+                        <InputText placeholder={"example"} /><br />
+                        <InputText />
+                    </div>
+
+                    <div className='center'>
+                        <Table deletion={"del"} data={data.users} add={obj => valueContext.setPopUp(<FormAddEditStudent newStudent={newStudent} UpdateStudent={UpdateStudent} />)} editing={obj => valueContext.setPopUp(<FormAddEditStudent userToUpdate={obj} UpdateStudent={UpdateStudent} newStudent={newStudent} />)} />
+                    </div>
+
+                </div>
+                    :
+                    <div className={styles.students}>
+                        <h1>פעילות לא נמצאה</h1>
+                    </div>}
+
+            </div>
         </div>
     );
 }
@@ -92,34 +97,5 @@ const Students = () => {
 
 
 
-//         // apiCalls.get(`/users/${activityId}`)
-//     }
-// }
-
-//     if (param >= 0 && param < valueContext.actions.length) {
-//         const activity = valueContext.actions[param];
-//         const activityType = activity.actionType;
-//         // console.log(activityType);
-//         const students = valueContext.users.filter(user => activity.users.includes(user.userId));
-
-//         return (
-
-//             <>
-//       <CurrentAction /><div>
-//                 <div className={`${styles.students}`}>
-//                     <PageHeader actionType={activityType} pageName={"תלמידים"} />
-//                     <Table deletion={"del"} data={students} add={obj => valueContext.setPopUp(<FormAddEditStudent newStudent={newStudent} UpdateStudent={UpdateStudent} />)} editing={obj => valueContext.setPopUp(<FormAddEditStudent userToUpdate={obj} UpdateStudent={UpdateStudent} newStudent={newStudent} />)} />
-//                 </div>
-//             </div></>
-//         );
-//     }
-//     else {
-//         return (
-//             <div className={styles.students}>
-//                 <h1>פעילות לא נמצאה</h1>
-//             </div>
-//         )
-//     }
-// }
 
 export default Students;
