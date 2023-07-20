71fa8be9aedde651a8ad32aceca5df272ac39281
Efraim <Nr224808@gmail.com>
2023-07-20 17:25:39 +0300

    stop for to sec

--- src/pages/Files/index.jsx
+++ src/pages/Files/index.jsx
@@ -1,16 +1,16 @@
 import React, { useContext, useEffect } from 'react';
 import styles from './style.module.css'
 import { useParams } from 'react-router-dom';
-import {DataContext} from '../../context/index'
+import { DataContext } from '../../context/index'
 
 const Files = () => {
     const params = useParams()
     const context = useContext(DataContext);
     const activety = context.isActive;
     const setActivety = context.setIsActive;
-    useEffect(()=>{
+    useEffect(() => {
         setActivety(true)
-    },[])
+    }, [])
     return (
         <div className={`center ${styles.Files}`}>
             files {params.id}page
--- src/pages/Tasks/index.jsx
+++ src/pages/Tasks/index.jsx
@@ -3,42 +3,47 @@ import styles from './style.module.css'
 import { useParams } from 'react-router-dom';
 import PageHeader from '../../components/PageHeader'
 import Table from '../../components/Table'
-import Popup from '../../layout/Popup';
 import apiCalls from '../../functions/apiCalls'
+import { DataContext } from '../../context/index'
+import FromToTasks from '../../components/FromToTasks';
+
 
 const Tasks = () => {
-    const [context, setContext] = useState()
+    const [data, setData] = useState()
+    const context = useContext(DataContext);
+    const params = useParams()
+
     const setActivety = context.setIsActive;
     useEffect(() => {
         setActivety(true)
     }, [])
 
     useEffect(() => {
-        apiCalls.get('/actions')
-            .then(res => setContext(res))
+        apiCalls.get(`/actions/${params.id}`)
+            .then(res => setData(res))
             .catch(errow => console.log(errow))
 
     }, [])
-
-
-
-
-    const params = useParams()
-    const allData = context.find(i => i._id === params.id).tasks
     const arr = ["isDone", "details", "deadline", "editing"]
-    console.log(allData);
-
-    console.log(allData.tasks);
+    console.log(data);
+    function Update(data) {
+        context.setPopUp(<FromToTasks data={data} />)
+    }
     return (
+        // <div><PageHeader pageName={"משימות לביצוע"} /*actionType={data.actionType}*/ /> </div>
         <div className={`center ${styles.tasks}`}>
-            <div><PageHeader pageName={"משימות לביצוע"} actionType={allData.actionType} /> </div>
-            <Table data={allData.tasks} arr={arr} /*editing={<Popup />}*/ deletion={<Popup />} TaskMarking={<Popup />} />
-            <div>{/* tasks {params.id} page */} </div>
-
+            {data ? <>
+                <Table data={data.tasks} arr={arr} editing={Update} deletion={"<Popup />"} TaskMarking={"<Popup />"} /> </> : null}
         </div>
     );
 }
 
+
 export default Tasks;
 
 
+
+
+
+
+// <div>{/* tasks {params.id} page */} </div>
